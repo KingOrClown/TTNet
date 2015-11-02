e81ee446b38bd3af31c9e9f0093558a3d28aa78c
KingOrClown <1015575651@qq.com>
2015-11-02 20:32:37 +0800

    初次建立,后续会丰富更新,别急

---
+++ rongtao/rongtao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rongtao
+//
+//  Created by 陈镕涛 on 15/11/2.
+//  Copyright © 2015年 陈镕涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rongtao/rongtao/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  rongtao
+//
+//  Created by 陈镕涛 on 15/11/2.
+//  Copyright © 2015年 陈镕涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
